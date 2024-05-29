56549144fe33486518937ca63ff2f61863b2d6ae
LI DAHUA <116102832+li-dahua@users.noreply.github.com>
2024-05-30 07:56:12 +0800

    Update follower_example.launch.py
    Using Primary to secondary pulse signal

--- follower_example.launch.py
+++ follower_example.launch.py
@@ -1,3 +1,20 @@
+# -----------------------------------------------------------------------------
+# Copyright 2024 Bernd Pfrommer <bernd.pfrommer@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+
 
 #
 # Example file for two Blackfly S cameras that are *externally triggered*, i.e
@@ -38,6 +55,7 @@ shared_cam_parameters = {
     'quiet': True,
     'buffer_queue_size': 1,
     'compute_brightness': True,
+    'pixel_format': 'BGR8',
     'exposure_auto': 'Off',
     'exposure_time': 10000,  # not used under auto exposure
     'gain_auto': 'Off',
